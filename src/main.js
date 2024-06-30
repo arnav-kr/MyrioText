cee373b490a690b3f17545d998239bccf48b5253
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-07-01 02:11:52 +0530

    feat: add tab change logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import './css/style.css';
 
+// modes
+document.querySelectorAll(".mode").forEach((element) => {
+  console.log(element)
+  element.addEventListener("input", (e) => {
+    if (e.target.value == "encode") {
+      document.body.classList.add("tab-encode");
+      document.body.classList.remove("tab-decode");
+    } else {
+      document.body.classList.add("tab-decode");
+      document.body.classList.remove("tab-encode");
+    }
+  });
+});
+
 // TODO: Uncomment the following lines to enable PWA Support
 // // PWAManager
 // import { PWAManager } from './js/PWAManager';
